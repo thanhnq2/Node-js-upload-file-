9f3b1816872280dea5cdd937dd19652dba07605e
thanhnq2 <53248599+thanhnq2@users.noreply.github.com>
2021-10-06 20:29:18 +0700

    Update server.js
    update lastest version

--- server.js
+++ server.js
@@ -114,6 +114,7 @@ app.listen(PORT, () => console.log(`Ser ver started on port ${PORT}`)) */
 const express = require('express')
 const bodyParser= require('body-parser')
 const multer = require('multer');
+
 const app = express();
 
 //CREATE EXPRESS APP
@@ -124,16 +125,75 @@ var storage = multer.diskStorage({
 	  cb(null, 'Uploads')
 	},
 	filename: function (req, file, cb) {
-	  cb(null, file.fieldname + '-' + Date.now())
+	  cb(null, file.originalname) // + '-' + Date.now())
 	}
   })
    
   var upload = multer({ storage: storage }) 
 //ROUTES WILL GO HERE
-app.get('/', function(req, res) {
-    res.json({ message: 'WELCOME' });   
+// app.get('/', function(req, res) {
+//     res.sendFile(__dirname + "/index.html");  
+// });
+// Upload file 
+var upload = multer({ storage : storage }).array('myFile',12);
+
+app.post('/uploadfiles',function(req,res){
+    upload(req,res,function(err) {
+        console.log(req.body);
+       // console.log(req.files);
+        if(err) {
+            return res.end("Error uploading file.");
+        }
+		//var filesarray = JSON.stringify(req.file.path);
+		//var file = req.files;
+		//console.log(filesarray);
+		//let fileupload = {
+		//	fname: req.files.pa
+		//}
+		var paths = req.files.map(file => file.path);
+		console.log(paths);
+
+		//result= result.substring(0, resultado.length - 1);  
+		//result= result + ']'
+		//res.contentType('application/json');
+		//var myJSONstring = JSON.stringify(resultado);
+		//console.log(result);
+	
+        db.collection('uploadfiles').insertOne({ "fnames": paths }, (err, result) => {
+			console.log(result);
+		
+			if (err) return console.log(err)
+		
+			console.log('saved to database')
+			res.redirect('/')
+			})
+    });
 });
-app.post('/uploadfile', upload.single('myFile'), (req, res, next) => {
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+app.get("/getfiles", function(req, res) {
+
+	db.collection('uploadfiles').findOne({ "pr_no": "01111010" }, (err, result) => {
+		//console.log(result);
+	
+		 if (err) return console.log(err)
+		
+		  //res.json(result);
+		  var a = result.fnames;
+		  console.log(typeof(a));
+		  
+		  return res.json(result);
+		  //var arrayToTable = require('array-to-table');
+		  //arrayToTable = a;
+		
+	 })
+
+    //res.json({ message: 'WELCOME' });   
+});
+/* app.post('/uploadfile', upload.array('myFile',10), (req, res) => {
 // Define a JSONobject for the image attributes for saving to database
   const file = req.file
   db.collection('uploadfiles').insertOne({ "fname": file.path }, (err, result) => {
@@ -143,16 +203,31 @@ app.post('/uploadfile', upload.single('myFile'), (req, res, next) => {
 
     console.log('saved to database')
     res.redirect('/')
-})
+	})
 
-/* 	const file = req.file
-	if (!file) {
-	  const error = new Error('Please upload a file')
-	  error.httpStatusCode = 400
-	  return next(error)
+}) */
+// update PR-PO workflow
+ const flowpo = [
+	{
+		prNo: 1,
+		poNo: 1,
+		sapPr: 1,
+		sapPo: 1
 	}
-	res.send(file) */
-  })
+]
+app.use(express.json())
+app.post('/flowpo', (req, res) => {
+	// res.json(posts.filter(post => post.userId === req.userId))
+	const flow_po = req.body;
+	db.collection('flow_po').insertOne(flow_po, (err, result) => {
+		console.log(result)
+	
+		if (err) return console.log(err)
+	
+		console.log(req.body)
+		res.redirect('/')
+		})
+})
  
   const MongoClient = require('mongodb').MongoClient
   const myurl = 'mongodb://localhost:27017';
