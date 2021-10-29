042a7b5bbb67ccae810afff68c4b74e1dab81045
thanhnq2 <nguyenquoc.thanh9@gmail.com>
2021-10-29 15:13:06 +0700

    update database

--- server.js
+++ server.js
@@ -562,4 +562,4 @@ client.connect((err, client) => {
 	});
 
 });
-app.listen(3000, () => console.log('Server started on port 3000'));
+app.listen(8018, () => console.log('Server started on port 8018'));
